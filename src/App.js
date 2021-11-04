f7aa8eb6ccaf199c4921bc2b48563a7f695cf0df
Alex Herrera <axherrerax@gmail.com>
2021-11-04 19:19:44 -0300

    Se agrega App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Lista from "./components/Lista";
+import Tarea from "./components/Tarea";
+import './App.css'
 
 function App() {
+  const [contador, setContador] = useState(0);
+  const [lista, setLista] = useState([]);
+  const [estadoEdicion, setEstadoEdicion] = useState(false);
+  const [elementoEditado, setelementoEditado] = useState("");
+
+  const agregarLista = (tarea) => {
+    setContador(contador + 1);
+    setLista([...lista, { id: contador, nombreTarea: tarea }]);
+  };
+
+  const elementoEliminado = (elemento) => {
+    setLista(elemento);
+  };
+
+  const editarElemento = (elemento) => {
+    setelementoEditado(elemento);
+  };
+
+  const nuevaLista = (nuevo) => {
+    setLista(nuevo);
+  };
+
+  const eliminarTodo = () => {
+    setLista([]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container mt-4">
+        <div className="row">
+          {/* TAREAS */}
+          <div className="col-5">
+            <h5 className="text-center">Agregar tarea</h5>
+
+            <Tarea
+              listado={lista}
+              agregarLista={agregarLista}
+              estadoEdicion={estadoEdicion}
+              elementoEditado={elementoEditado}
+              nuevaLista={nuevaLista}
+              setEstadoEdicion={setEstadoEdicion}
+            />
+          </div>
+
+          {/*LISTA DE TAREAS */}
+          <div className="col-7">
+            <h5 className="text-center">Lista de tareas</h5>
+            <Lista
+              listado={lista}
+              elementoEliminado={elementoEliminado}
+              editarElemento={editarElemento}
+              estadoEdicion={setEstadoEdicion}
+              eliminarTodo={eliminarTodo}
+            />
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
